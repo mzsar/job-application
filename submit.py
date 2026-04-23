50aa9b3349e1c1d92ad29456911ff44f00cd296b
mzsar <martasanchez2628@gmail.com>
2026-04-23 21:32:22 +0200

    Create submit.py
    frist commit

---
+++ submit.py
@@ -0,0 +1,11 @@
+import requests
+import json
+import hmac
+import hashlib
+from datetime import datetime
+
+payload ={
+    "action_run_link": "",
+    "email": "martasanchez2628@gmail.com",
+    "name": "Marta Sanchez Martin",
+}
