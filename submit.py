9c8a2fd29826d6fd0b23ac1c3a83b6c64dcc43b4
mzsar <martasanchez2628@gmail.com>
2026-04-23 22:20:48 +0200

    Update submit.py
    ch

--- submit.py
+++ submit.py
@@ -12,24 +12,28 @@ payload ={
     # if you're watching this, here's my other repo -> https://github.com/MkS2244 
     # this one contains one of my personal projects :)
     "repository_link": "https://github.com/mzsar/job-application",
-    "action_run_link": "https://github.com/mzsar/job-application/actions/runs/ID"
+    "action_run_link": "https://github.com/mzsar/job-application/actions/runs/24856851626"
 }
 
 body = json.dumps(payload, separators=(",",":"), sort_keys=True).encode("utf-8")
 signature = hmac.new(
-    b"Hello-you-all",
+    b"hello-there-from-b12",
     body,
     hashlib.sha256
 ).hexdigest()
 
+print(body.decode("utf-8"))
+print(signature)
+
 response = requests.post(
     "https://b12.io/apply/submission",
     data=body,
-    headers = {
+    headers={
+        "Content-Type": "application/json",
         "X-Signature-256": f"sha256={signature}",
-        "Content-Type": "application/json"
-    }
+    },
+    timeout=30,
 )
 
-response.raise_for_status()
-print(response.json()["receipt"])
+print(response.status_code)
+print(response.text)
